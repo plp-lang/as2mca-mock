29341ff157cde669218c46be69162f286bfd3836
Falldot <fe.offep@gmail.com>
2026-06-18 00:33:27 +1100

    chore: bump deps and add shadow-rs

--- src/config/args.rs
+++ src/config/args.rs
@@ -1,6 +1,9 @@
 use clap::Parser;
 use clap::ValueEnum;
 use serde::{Deserialize, Serialize};
+use shadow_rs::shadow;
+
+shadow!(build);
 
 #[derive(Clone, Debug, ValueEnum, Serialize, Deserialize)]
 pub enum LogFormat {
@@ -13,23 +16,23 @@ pub enum LogFormat {
 #[command(author, version, long_about = None)]
 pub struct Args {
   /// Наименование приложения, префикс для всех HTTP запросов. Повторяет поведение TOMCAT.
-  #[arg(short, long, env = "PLP_MOCKS_WEB_APP_NAME", default_value = "platform2mca")]
+  #[arg(short, long, env = "AS2MCA_MOCKS_WEB_APP_NAME", default_value = "platform2mca")]
   pub web_app_name: Box<str>,
 
   /// Хост сервера
-  #[arg(long, env = "PLP_MOCKS_HOST", default_value = "0.0.0.0")]
+  #[arg(long, env = "AS2MCA_MOCKS_HOST", default_value = "0.0.0.0")]
   pub host: Box<str>,
 
   /// Порт сервера
-  #[arg(short, long, env = "PLP_MOCKS_PORT", default_value_t = 3000)]
+  #[arg(short, long, env = "AS2MCA_MOCKS_PORT", default_value_t = 3000)]
   pub port: u16,
 
   /// Формат логирования
-  #[arg(long, env = "PLP_MOCKS_LOG_FORMAT", default_value = "compact")]
+  #[arg(long, env = "AS2MCA_MOCKS_LOG_FORMAT", default_value = "compact")]
   pub log_format: LogFormat,
 
   /// Уровень логирования
-  #[arg(long, env = "PLP_MOCKS_LOG_FILTER")]
+  #[arg(long, env = "AS2MCA_MOCKS_LOG_FILTER")]
   pub log_filter: Option<String>,
 
   #[arg(env = "RUST_LOG", default_value = "info")]
